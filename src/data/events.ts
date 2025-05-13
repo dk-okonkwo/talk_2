e4f78f16a1ef4484ea06c5ba01d892d99c2a0145
dkokonkwo <d.okonkwo@alustudent.com>
2025-05-12 22:06:34 -0700

    image url test 2

---
+++ src/data/events.ts
@@ -0,0 +1,62 @@
+export interface MainEvent {
+  id: number
+  img: string
+  title: string
+  date: string
+  time: string
+  entry: string
+  description: string
+}
+
+export const allEvents: MainEvent[] = [
+  {
+    id: 1,
+    img: '@/assets/images/events/event1.png',
+    title: 'Soccer Tournament',
+    date: 'Friday - 17/08',
+    time: '9:30am',
+    entry: 'Free',
+    description:
+      'Event Description. Maecenas scelerisque, arcu quis tempus egestas, ligula diam molestie lectus, tincidunti.',
+  },
+  {
+    id: 2,
+    img: '../assets/images/events/event2.png',
+    title: 'Semi Finals - Swimming',
+    date: 'Saturday - 19/09',
+    time: '9:30am',
+    entry: 'Free',
+    description:
+      'Mauris scelerisque odio id rutrum volutpat. Pellentesque urna odio, vulputate at tortor vitae, hendrerit blandit lorem.',
+  },
+  {
+    id: 3,
+    img: '@/assets/images/events/event3.png',
+    title: 'Gymnastics Competitions',
+    date: 'Sunday - 21/03',
+    time: '9:30am',
+    entry: 'Free',
+    description:
+      'Pellentesque vestibulum lorem vel gravida aliquam. Morbi porta, odio id suscipit mattis, risus augue condimentum purus.',
+  },
+  {
+    id: 4,
+    img: 'src/assets/images/events/event4.png',
+    title: 'Judo - Training',
+    date: 'Wednesday - 21/08',
+    time: '12:30pm',
+    entry: 'Free',
+    description:
+      'Pellentesque vestibulum lorem vel gravida aliquam. Morbi porta, odio id suscipit mattis, risus augue condimentum purus.',
+  },
+  {
+    id: 5,
+    img: 'src/assets/images/events/event5.png',
+    title: 'Fit Dance in the sqaure',
+    date: 'Teusday - 22/03',
+    time: '8:00am',
+    entry: 'Free',
+    description:
+      'Mauris scelerisque odio id rutrum volutpat. Pellentesque urna odio, vulputate at tortor vitae, hendrerit blandit lorem. ',
+  },
+]
